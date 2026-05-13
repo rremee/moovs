8029c7af409ba1ee805804f19c7cf14af3c0db91
Anton Valkovskyi <u1remited@gmail.com>
2026-05-13 16:23:19 +0300

    feat: add constants for navigation menu

---
+++ src/constants/script.js
@@ -0,0 +1,11 @@
+const menuItems = [
+	{id:'nav-item-1', name: 'Home', icon: 'icon-home', path: '/'},
+	{id:'nav-item-2', name: 'Movies', icon: 'icon-movies', path: '/'},
+	{id:'nav-item-3', name: 'Series', icon: 'icon-series', path: '/'},
+	{id:'nav-item-4', name: 'Trending', icon: 'icon-trending', path: '/'},
+	{id:'nav-item-5', name: 'Coming soon', icon: 'icon-coming', path: '/'},
+	{id:'nav-item-6', name: 'Actors', icon: 'icon-actor', path: '/'},
+	{id:'nav-item-7', name: 'Favorites', icon: 'icon-favorite', path: '/'},
+]
+
+export {menuItems};
