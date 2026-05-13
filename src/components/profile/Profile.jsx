1fe91213f911241dd4acc358228411830745d5ca
Anton Valkovskyi <u1remited@gmail.com>
2026-05-13 16:26:34 +0300

    feat: init Profile component

---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,16 @@
+import './profile.scss'
+
+const Profile = () => {
+	return (
+		<a href="/" className="profile">
+			<div className="profile__avatar">
+
+			</div>
+			<div className="profile__name">
+				Profile Name
+			</div>
+		</a>
+	);
+};
+
+export default Profile;
