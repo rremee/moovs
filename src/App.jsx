f0a8fdacf7539d6bdc4f3613f4f55482cbebce32
Anton Valkovskyi <u1remited@gmail.com>
2026-05-15 10:12:19 +0300

    feat: add missing App components and set up global app styles (grid, sections)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import Sidebar from "./components/sidebar/Sidebar.jsx";
+import MovieCard from "./components/movieCard/MovieCard.jsx";
+import Heading from "./components/heading/Heading.jsx";
+import UpcomingList from "./components/UpcomingList/UpcomingList.jsx";
+import NewsHome from "./components/NewsHome/NewsHome.jsx";
 
 function App() {
 
   return (
     <div className={'app-main'}>
-      <Sidebar/>
+        <Sidebar/>
+        <main className="main-content">
+            <section className={'home-slider'}>
+                <Heading name={'Now Playing'} />
+                <MovieCard/>
+            </section>
+            <div className="news-wrapper">
+                <aside className={'news'}>
+                    <Heading name={'News'} />
+                    <NewsHome/>
+                </aside>
+            </div>
+            <section className={'upcoming'}>
+                <Heading name={'Upcoming releases'} />
+                <UpcomingList/>
+            </section>
+        </main>
     </div>
   )
 }
