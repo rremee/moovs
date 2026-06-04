dfe45ac67889f8137b6f25357849a2b89da7d664
Anton Valkovskyi <u1remited@gmail.com>
2026-06-04 08:52:35 +0300

    feat(movie-card): make component presentational and add skeleton loading

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,7 +1,6 @@
-import {useEffect, useState} from "react";
 import MovieDetail from "../MovieDetail/MovieDetail.jsx";
+import Skeleton from 'react-loading-skeleton';
 import ActorHomeSlider from "../ActorHomeSlider/ActorHomeSlider.jsx";
-import useTMDBService from "../../services/TMDBService.js";
 import './movieCard.scss';
 
 const truncText = (text, maxLength) => {
@@ -11,69 +10,75 @@ const truncText = (text, maxLength) => {
 	return text.slice(0, lastSpace > 0 ? lastSpace : maxLength) + '...';
 }
 
-const MovieCard = ({movie, isActive}) => {
+const MovieCard = ({movie, isActive, isLoading}) => {
 
-	const [actors, setActors] = useState([]);
-	const [details, setDetails] = useState([]);
-	if (!movie) return null;
-	const {id, title, overview, image} = movie;
-	const {getMoviesPlayingDetails} = useTMDBService();
-
-	useEffect(() => {
-		if (id) {
-			getMoviesPlayingDetails(id)
-				.then(data => {
-					setActors(data.actors);
-					setDetails(data.detailsList);
-				})
-				.catch(err => console.error("Error movie details loading:", err));
-		}
-	}, [id])
+	const { title, overview, image, actors = [], detailsList = [] } = movie || {};
 
 	return (
-		<div className={`movie-card ${isActive ? 'movie-card--active' : ''}`}>
-			<img src={image} alt={title} className="movie-card__img"/>
+		<div className={`movie-card ${isActive ? 'movie-card--active' : ''} ${isLoading ? 'disabled' : ''}`}>
+
+			{
+				isLoading
+					? (
+						<div className="movie-card__img">
+							<Skeleton height="100%" />
+						</div>
+					)
+					: <img src={image} alt={title} className="movie-card__img"/>
+			}
+
 			<div className="movie-card__details">
-				{details.map(movie => (
-					<MovieDetail
-						key={movie.id}
-						name={movie.name}
-					/>
-				))}
+				{
+					isLoading
+					? Array.from({ length: 3 }, (_, i) => <Skeleton key={i} width={60} height={24}/>)
+					: detailsList.map(item => (
+							<MovieDetail
+								key={item.id}
+								name={item.name}
+							/>
+						))
+				}
 			</div>
+
 			<div className="movie-card__actors">
-				{actors.map(actor => (
-					<ActorHomeSlider
-						key={actor.id}
-						name={actor.name}
-						imageSrc={actor.imageSrc}
-					/>
-				))}
+				{
+					isLoading
+					? Array.from({ length: 3 }, (_, i) => (
+						<div key={i} className="movie-card__actors--skeleton">
+							<Skeleton width={82} height={20} />
+							<Skeleton width={42} height={60} />
+						</div>
+					))
+					: actors.map(actor => (
+							<ActorHomeSlider
+								key={actor.id}
+								name={actor.name}
+								imageSrc={actor.imageSrc}
+							/>
+						))
+				}
 			</div>
+
 			<div className="movie-card__content">
 				<h3 className="movie-card__title">
-					{title}
+					{isLoading ? <Skeleton width='60%'/> : title}
 				</h3>
 				<p className="movie-card__descr">
-					{truncText(overview, 160)}
+					{isLoading ? <Skeleton count={3} width='90%'/> : truncText(overview, 160)}
 				</p>
 				<div className="movie-card__actions">
 					<ul>
-						<li>
-							<a href="">
-								<span className={'icon-play'}></span>
-							</a>
-						</li>
-						<li>
-							<a href="">
-								<span className={'icon-star'}></span>
-							</a>
-						</li>
-						<li>
-							<a href="">
-								<span className={'icon-favorite'}></span>
-							</a>
-						</li>
+						{
+							isLoading
+							? Array.from({ length: 3 }, (_, i) => <li key={i}><Skeleton circle width={24} height={24}/></li>)
+							: (
+								<>
+									<li><a href=""><span className={'icon-play'}></span></a></li>
+									<li><a href=""><span className={'icon-star'}></span></a></li>
+									<li><a href=""><span className={'icon-favorite'}></span></a></li>
+								</>
+							)
+						}
 					</ul>
 				</div>
 			</div>
